cbcffbacc4421bf7f01fca33353e3536fd41d691
bernardobb <bernardobb.eletronica@gmail.com>
2022-09-27 15:22:23 -0300

    webview direto para o github

--- App.js
+++ App.js
@@ -10,37 +10,7 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar hidden/>
     <WebView 
-    source={{ html: `
-    <html>
-    <head>
-    <meta name="viewport" content="width=device-width", initial-scale=1.0, maximum-scale=1.0>
-    </head>
-      <body>
-      <style>
-      *{
-        margin:0;
-        padding:0;
-        box-sizing:border-box;
-      }
-      .btn{
-        heidht:20px;
-        width:100%;
-        background-color:#069;
-        text-align:center;
-        color:white;
-      }
-      
-      </style>
-      <div id="btn" class="btn">Clique aqui</div>
-      <script>
-      var el=document.getElementById('btn');
-      el.addEventListener('click',()=>{
-        alert('Clicou!');
-      });
-      </script>
-      </body>
-    </html>
-    ` }}
+    source={{ uri: 'https://github.com/bernardobb93'}}
   />
     </View>
   );
