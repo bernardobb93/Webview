c25bb6e9bf990ebb82802c88f94ba42819ec5aa7
bernardobb <bernardobb.eletronica@gmail.com>
2022-09-27 15:16:56 -0300

    teste simples

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden/>
+    <WebView 
+    source={{ html: `
+    <html>
+    <head>
+    <meta name="viewport" content="width=device-width", initial-scale=1.0, maximum-scale=1.0>
+    </head>
+      <body>
+      <style>
+      *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+      }
+      .btn{
+        heidht:20px;
+        width:100%;
+        background-color:#069;
+        text-align:center;
+        color:white;
+      }
+      
+      </style>
+      <div id="btn" class="btn">Clique aqui</div>
+      <script>
+      var el=document.getElementById('btn');
+      el.addEventListener('click',()=>{
+        alert('Clicou!');
+      });
+      </script>
+      </body>
+    </html>
+    ` }}
+  />
     </View>
   );
 }
@@ -13,8 +49,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
